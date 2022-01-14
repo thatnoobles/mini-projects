02fc31f8852ca5f0b184cb4f77b10ef9cea94064
thatnoobles <danildonchuk@gmail.com>
2022-01-14 10:49:07 -0500

    added FallingApples and GreatValueWordle

---
+++ FallingApples/Program.cs
@@ -0,0 +1,207 @@
+﻿#pragma warning disable CA1416	// SetWindowSize() warning about possibly breaking cross-platform, don't need to worry about it
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace FallingApples
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.SetWindowSize(8, 34);
+			Console.CursorVisible = false;
+			Game.Start();
+			Console.Read();
+		}
+	}
+
+	public class Game
+	{
+		public static char[,] board = new char[32, 8];
+
+		private static int frame = 0;
+		private static int score = 0;
+		private static Random random = new Random();
+		private static bool gameOver = false;
+		private static int spawnWait = 60;	// (In frames) the delay between apple spawns. Lower value = faster spawning
+
+		public static void Start()
+		{
+			Console.Clear();
+
+			for (int i = 0; i < board.GetLength(0); i++)
+			{
+				for (int j = 0; j < board.GetLength(1); j++)
+				{
+					board[i, j] = '.';
+				}
+			}
+
+			board[31, 0] = '~';
+			Update();
+		}
+
+		private static void Update()
+		{
+			while (true)
+			{
+				if (Console.KeyAvailable)	// Read input without blocking
+				{
+					ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+					if (keyInfo.Key == ConsoleKey.LeftArrow)
+					{
+						if (GetPlayerPosition() != 0)
+						{
+							int currentPlayerPos = GetPlayerPosition();
+
+							board[31, currentPlayerPos] = '.';
+							board[31, currentPlayerPos - 1] = '~';
+						}
+					}
+					else if (keyInfo.Key == ConsoleKey.RightArrow)
+					{
+						if (GetPlayerPosition() != board.GetLength(1) - 1)
+						{
+							int currentPlayerPos = GetPlayerPosition();
+
+							board[31, currentPlayerPos] = '.';
+							board[31, currentPlayerPos + 1] = '~';
+						}
+					}
+				}
+
+				// Spawn an apple every 1 second
+				if (frame % spawnWait == 0)
+				{
+					SpawnApple();
+				}
+
+				// Move apples every .5 seconds
+				if (frame % 2 == 0)
+				{
+					UpdateApples();
+				}
+
+				Display();
+				Thread.Sleep(16);	// approx. 60 fps
+
+				if (gameOver)
+				{
+					Console.Clear();
+					Console.SetWindowSize(80, 2);
+					Console.Write($"Game over! Your score was {score}. Press enter to quit.");
+					return;
+				}
+				
+				frame++;
+			}
+		}
+
+		private static void Display()
+		{
+			for (int i = 0; i < board.GetLength(0); i++)
+			{
+				Console.SetCursorPosition(0, i);
+
+				for (int j = 0; j < board.GetLength(1); j++)
+				{
+					Console.ForegroundColor = board[i, j] switch
+					{
+						'~' => ConsoleColor.Yellow,
+						'O' => ConsoleColor.Red,
+						_ => ConsoleColor.DarkGray
+					};
+
+					Console.Write(board[i, j]);
+				}
+
+				Console.Write("\n");
+			}
+
+			Console.ResetColor();
+			Console.SetCursorPosition(0, 32);
+			Console.WriteLine(score);
+		}
+	
+		private static int GetPlayerPosition()
+		{
+			for (int i = 0; i < Game.board.GetLength(1); i++)
+			{
+				if (board[31, i] == '~')
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+	
+		private static void SpawnApple()
+		{
+			board[0, random.Next(0, board.GetLength(1))] = 'O';
+		}
+	
+		private static void UpdateApples()
+		{
+			foreach (int i in FindRowsWithApples())
+			{
+				for (int j = 0; j < board.GetLength(1); j++)
+				{
+					if (board[i, j] == 'O')
+					{
+						if (i + 1 < board.GetLength(0))
+						{
+							switch (board[i + 1, j])
+							{
+								case '.':
+									board[i, j] = '.';
+									board[i + 1, j] = 'O';
+									break;
+
+								case '~':
+									board[i, j] = '.';
+									score++;
+
+									// Double speed every 5 points
+									if (score % 5 == 0)
+									{
+										spawnWait /= 2;
+									}
+									
+									break;
+
+								default:
+									break;
+							}
+						}
+						else
+						{
+							gameOver = true;
+						}
+					}
+				}	
+			}
+		}
+
+		private static List<int> FindRowsWithApples()
+		{
+			List<int> output = new List<int>();
+
+			for (int i = 0; i < board.GetLength(0); i++)
+			{
+				for (int j = 0; j < board.GetLength(1); j++)
+				{
+					if (board[i, j] == 'O')
+					{
+						output.Add(i);
+					}
+				}
+			}
+
+			return output;
+		}
+	}
+}
---
+++ GreatValueWordle/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+
+namespace GreatValuewordle
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.Clear();
+			new Game();
+		}
+	}
+
+	public class Game
+	{
+		private string word = "";
+		private const int TOTAL_GUESSES = 6;
+		private int remainingGuesses = TOTAL_GUESSES;
+		private string[] guesses = new string[TOTAL_GUESSES]; 
+		private string[] wordList = File.ReadAllLines("words.txt");
+
+		public Game()
+		{
+			Random random = new Random();
+
+			word = wordList[random.Next(0, 1572)];	// 1572 is a totally arbitrary number lmao
+			Start();
+		}
+
+		private void Start()
+		{
+			Console.WriteLine("Welcome to discount Wordle. You know the rules.");
+			Console.WriteLine("Press enter to continue...");
+			Console.Read();
+			Update();
+		}
+
+		private void Update()
+		{
+			while (remainingGuesses > 0)
+			{
+				Console.Clear();
+
+				foreach (string word in guesses)
+				{
+					if (word != null)
+					{
+						for (int i = 0; i < word.Length; i++)
+						{
+							PrintLetterColor(word, word[i], i);
+						}
+
+						Console.Write("\n");
+					}
+				}
+
+				Console.Write($"\nGuess a 5-letter word ({remainingGuesses} guesses left): ");
+				string guess = Console.ReadLine().Trim().ToLower();
+
+				if (guess.Length != 5 || !InWordList(guess))
+				{
+					continue;
+				}
+
+				if (guess == word)
+				{
+					Console.ForegroundColor = ConsoleColor.Green;    
+					Console.WriteLine($"\nYou win! The word was {word}. Press enter to exit.");
+					Console.ResetColor();
+					Console.Read();
+					Console.Clear();
+					return;
+				}
+
+				guesses[remainingGuesses - 1] = guess;
+				remainingGuesses--;
+			}
+
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine($"\nYou're out of guesses! The word was {word}. Press enter to exit.");
+			Console.ResetColor();
+			Console.Read();
+			Console.Clear();
+		}
+
+		private bool InWordList(string word)
+		{
+			foreach (string s in wordList)
+			{
+				if (s.Trim() == word)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	
+		private void PrintLetterColor(string guess, char letter, int index)
+		{
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+
+			if (word.Contains(letter))
+			{
+				if (guess[index] == word[index])
+				{
+					Console.ForegroundColor = ConsoleColor.Green;
+				}
+				else
+				{
+					Console.ForegroundColor = ConsoleColor.Yellow;
+				}
+			}
+
+			Console.Write(letter);
+			Console.ResetColor();
+		}
+	}
+}
